ab7a5c3c3eb6dcaa2c91ec4a98c12f950ae00f2d
Vigrel <eller.vini@gmail.com>
2021-09-27 12:39:08 -0300

    build: adiciona tudo para conceito C+

--- Projeto 1B/notes/models.py
+++ Projeto 1B/notes/models.py
@@ -7,4 +7,4 @@ class Note(models.Model):
     id = models.AutoField(primary_key=True)
     
     def __str__(self) -> str:
-        return f'{self.id}, {self.title}'
+        return f'{self.id}.{self.title}'
--- Projeto 1B/notes/views.py
+++ Projeto 1B/notes/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Note
 
-
 def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
@@ -9,7 +8,30 @@ def index(request):
         note = Note(title = title, content = content)
         note.save()
         return redirect('index');
-
+    
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(req):
+    id = req.POST.get('delete')
+    note = Note.objects.get(id=int(id))
+    note.delete()
+    return redirect('index');
+
+
+def put(req):
+    note_req = req.GET.get('put')
+    dict = {}
+
+    for i in note_req.split('&'):
+        k = i.split('=')[0]
+        v = i.split('=')[1]
+        dict[k] = v
+
+    note = Note.objects.get(id=int(dict["id"]))
+    note.title = dict["title"]
+    note.content = dict["content"]
+    note.save()
+
+    return redirect('index')
